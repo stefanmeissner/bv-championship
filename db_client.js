d63448e71b7f345874e8e0e17ad1c9c4696e65c6
Stefan Meissner <stefan.meissner@tui.com>
2018-06-11 16:57:36 +0200

    load points from file

---
+++ db_client.js
@@ -0,0 +1,37 @@
+function getFileFromServer(url, doneCallback) {
+    var xhr;
+
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = handleStateChange;
+    xhr.open("GET", url, true);
+    xhr.send();
+
+    function handleStateChange() {
+        if (xhr.readyState === 4) {
+            doneCallback(xhr.status == 200 ? xhr.responseText : null);
+        }
+    }
+}
+
+function init() {
+    getFileFromServer("https://raw.githubusercontent.com/RobertPazurek/bv-championship/master/points_meisi.txt", function(text) {
+        if (text === null) {
+            // An error occurred
+        }
+        else {
+            document.getElementById("points_meisi").innerText = text;
+        }
+    });
+
+    getFileFromServer("https://raw.githubusercontent.com/RobertPazurek/bv-championship/master/points_rob.txt", function(text) {
+        if (text === null) {
+            // An error occurred
+        }
+        else {
+            document.getElementById("points_rob").innerText = text;
+        }
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', init, false);
